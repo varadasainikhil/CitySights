4e1514e19c07e0766c094423167a92c2c154b445
varadasainikhil <varadasainikhil@gmail.com>
2025-05-05 17:26:07 -0400

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Sai Nikhil Varada on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
