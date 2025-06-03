b8dfd19cb3f57e2b88aabce910d9bbc5af02273b
varadasainikhil <varadasainikhil@gmail.com>
2025-06-03 16:30:14 -0400

    - pass info of the business to the businesdetailview - hookup selection in list view

---
+++ CitySights/BusinessDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  BusinessDetailView.swift
+//  CitySights
+//
+//  Created by Sai Nikhil Varada on 6/3/25.
+//
+
+import SwiftUI
+
+struct BusinessDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BusinessDetailView()
+}
